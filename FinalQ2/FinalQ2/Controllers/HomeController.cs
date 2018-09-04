dcf46a7d4956b3d9c231f96bdf2652d86aed2612
Mozzey <mbcacciano@gmail.com>
2018-09-04 13:56:13 -0400

    Final Assessment Part 2 Q2

---
+++ FinalQ2/FinalQ2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalQ2.ViewModels;
+using FinalQ2.DAL;
+
+namespace FinalQ2.Controllers
+{
+    public class HomeController : Controller
+    {
+        NorthwindDB db = new NorthwindDB();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult CustomerView(CustomerViewModel customer)
+        {
+            return View(customer);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ FinalQ2/FinalQ2/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FinalQ2.ViewModels
+{
+    public class CustomerViewModel
+    {
+        public string CustomerID { get; set; }
+
+        public string CompanyName { get; set; }
+    }
+}
